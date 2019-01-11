eff1248958b9ad1afb8c36f4e6ee94a954c4400b
Sam <samdup123@gmail.com>
2019-01-11 09:45:13 -0500

    list is not a real git command

--- git/add_git_aliases.sh
+++ git/add_git_aliases.sh
@@ -5,7 +5,7 @@ git config --global alias.ca 'commit -am'
 git config --global alias.a 'add .'
 git config --global alias.s status
 git config --global alias.d diff
-git config --global alias.l list
+git config --global alias.l log
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.co checkout
 git config --global alias.b branch
