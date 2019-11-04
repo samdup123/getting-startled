585b86a0816b59157d569e14e24532136415b555
Sam <samdup123@gmail.com>
2019-11-04 08:04:12 -0800

    ' => "

--- git/add_git_aliases.sh
+++ git/add_git_aliases.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 git config --global alias.c commit
-git config --global alias.cm 'commit -m'
-git config --global alias.a 'add .'
+git config --global alias.cm "commit -m"
+git config --global alias.a "add ."
 git config --global alias.s status
 git config --global alias.d diff
 git config --global alias.l log
-git config --global alias.unstage 'reset HEAD --'
-git config --global alias.u 'reset HEAD --'
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.u "reset HEAD --"
 git config --global alias.co checkout
 git config --global alias.b branch
 git config --global alias.alias "config --get-regexp ^alias\."
